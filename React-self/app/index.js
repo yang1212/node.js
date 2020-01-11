20888deb83743192685263456d8369d5ef8567a4
Unknown <1204914685@qq.com>
2020-01-12 01:08:44 +0800

    Push  webpack版本升级v1

--- React-self/app/index.js
+++ React-self/app/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import IndexApp from './containers'
 import {render} from 'react-dom'
-import {AppContainer} from 'react-hot-loader'
 
 let div = document.createElement('div');
 div.setAttribute('id', 'app');
@@ -10,13 +9,6 @@ document.body.appendChild(div);
 const mountNode = document.getElementById('app');
 
 render(
-    <AppContainer>
-        <IndexApp/>
-    </AppContainer>
-    ,
+    <IndexApp/>,
     mountNode
 );
-
-if(module.hot && process.env.NODE_ENV !== 'production'){
-    module.hot.accept();
-}
