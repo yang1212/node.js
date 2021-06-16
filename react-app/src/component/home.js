4e35208d116c37e41524551d6adcb347ec12a2e1
yang_fu <yangfu@i72.com>
2021-06-16 16:05:53 +0800

    feat: 添加ant

--- react-app/src/component/home.js
+++ react-app/src/component/home.js
@@ -1,16 +1,15 @@
 import * as React from 'react'
+import { Button } from 'antd'
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props)
-    console.log(2, props)
-  }  
   componentDidMount() {
     console.log(1, this.props.children)
   }
   render() {
     return (
-      <div>home</div>
+      <div>
+        <Button type="primary">Primary</Button>
+      </div>
     )
   }
 }
